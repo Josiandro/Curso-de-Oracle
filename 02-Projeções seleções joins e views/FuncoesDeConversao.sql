5aa636abcfbc93938f629eecb034cf3b3d75fa9b
Josiandro <josiandro.santossilveira@hotmail.com>
2022-12-09 15:42:32 -0300

    Update FuncoesDeConversao.sql

--- 02-Projeções seleções joins e views/FuncoesDeConversao.sql
+++ 02-Projeções seleções joins e views/FuncoesDeConversao.sql
@@ -78,3 +78,63 @@ FULL JOIN
 ON TV.BAIRRO = TC.BAIRRO;
 
 --------------------------------------------------------------------------------
+
+-- Queremos construir um SQL cujo resultado seja, para cada cliente:
+-- O cliente NOME DO CLIENTE faturou QUANTIDADE no ano de ANO
+-- Faça isso somente para o ano de 2016.
+SELECT
+    'O cliente ' || CLIENTES.NOME || 
+    ' faturou ' || ROUND(VENDAS.QUANTIDADE, 2) || 
+    ' no ano de ' || VENDAS.DATA
+FROM
+    TABELA_DE_CLIENTES CLIENTES
+INNER JOIN
+    (SELECT
+        NOTAS.CPF AS CPF,
+        TO_CHAR(NOTAS.DATA_VENDA, 'YYYY') AS DATA,
+        SUM(ITENS.QUANTIDADE * ITENS.PRECO) AS QUANTIDADE
+    FROM
+        NOTAS_FISCAIS NOTAS
+    INNER JOIN
+        ITENS_NOTAS_FISCAIS ITENS
+    ON NOTAS.NUMERO = ITENS.NUMERO
+    WHERE TO_CHAR(NOTAS.DATA_VENDA, 'YYYY') = 2016
+    GROUP BY CPF, TO_CHAR(NOTAS.DATA_VENDA, 'YYYY')
+    ORDER BY QUANTIDADE DESC) VENDAS
+ON CLIENTES.CPF = VENDAS.CPF;
+
+--------------------------------------------------------------------------------
+
+-- Exercício usando dois INNER JOIN (Minha resolução)
+SELECT
+    'O cliente ' || TB_CLI.NOME ||
+    ' faturou ' || ROUND(SUM(TB_ITNF.PRECO * TB_ITNF.QUANTIDADE),2) ||
+    ' no ano de ' || TO_CHAR(TB_NF.DATA_VENDA, 'YYYY')
+FROM
+    TABELA_DE_CLIENTES TB_CLI
+INNER JOIN
+    NOTAS_FISCAIS TB_NF
+ON TB_CLI.CPF = TB_NF.CPF
+INNER JOIN
+    ITENS_NOTAS_FISCAIS TB_ITNF
+ON TB_NF.NUMERO = TB_ITNF.NUMERO
+WHERE TO_CHAR(TB_NF.DATA_VENDA, 'YYYY') = 2016
+GROUP BY TB_CLI.NOME, TO_CHAR(TB_NF.DATA_VENDA, 'YYYY');
+--------------------------------------------------------------------------------
+
+-- Resolução da ALURA (fez INNER JOIN com 3 tabelas)
+SELECT 
+    'O cliente ' || TC.NOME ||
+    ' faturou ' || TO_CHAR(ROUND(SUM(INF.QUANTIDADE * INF.preco),2)) || 
+    ' no ano de ' || TO_CHAR(DATA_VENDA, 'YYYY') AS SENTENCA 
+FROM 
+    notas_fiscais NF
+INNER JOIN 
+    ITENS_NOTAS_FISCAIS INF 
+ON NF.NUMERO = INF.NUMERO
+INNER JOIN 
+    TABELA_DE_CLIENTES TC 
+ON NF.CPF = TC.CPF
+WHERE TO_CHAR(DATA_VENDA, 'YYYY') = '2016'
+GROUP BY TC.NOME, TO_CHAR(DATA_VENDA, 'YYYY');
+--------------------------------------------------------------------------------
